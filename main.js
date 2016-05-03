673c119fc5f7e44e6e42ec35cdee1780c9d6734d
jungkyunghwan <sujkh@nomadconnection.com>
2016-05-03 14:57:00 +0900

    update effect up down right left

--- main.js
+++ main.js
@@ -7,18 +7,26 @@ var Main = React.createClass({
   actionChildren2(){
     console.log('actionChildren2');
   },
-  //floatButtonSetting please only touch buttonDistance,speed
+  //floatButtonSetting
   FBS:{
-    //autoSetting please don't touch 
+    //autoSetting please don't touch
     showFlag:false,
     isFirst : true,
     tag : {},
     target : {},
     top : 0,
+    left : 0,
     childrenLength : 0,
     //userSetting
     buttonDistance : 80,
-    speed :500
+    speed :500,
+    buttonStyle:{
+      width:60,
+      height:60,
+      fontSize:30
+    },
+    //up down right left effect
+    effect:'up'
   },
   actionFloatButton(event){
     if(this.FBS.isFirst){
@@ -27,24 +35,62 @@ var Main = React.createClass({
       this.FBS.target = $(this.FBS.tag);
       this.FBS.top
         = this.FBS.target.offset().top;
+      this.FBS.left = this.FBS.target.offset().left;
       this.FBS.childrenLength
         = this.FBS.tag.parentNode.childNodes.length;
     }
 
     var count = 1;
 
+    //
     if(!this.FBS.showFlag){
       this.FBS.showFlag = true;
 
       for(count ; count < this.FBS.childrenLength; count++){
-        var targetTop = this.FBS.top - this.FBS.buttonDistance * count;
-        var child = this.FBS.tag.parentNode.childNodes[count];
+        //effect up
+        var location ={};
+        var child ={};
+        if('up' === this.FBS.effect){
 
-        $(child).css('top',$(child).offset().top).css('z-index',100-count);
+          location = this.FBS.top - this.FBS.buttonDistance * count;
+          child = this.FBS.tag.parentNode.childNodes[count];
+          $(child).css('top',$(child).offset().top).css('z-index',100-count);
+          //effect up down
+          $(child).stop().animate({
+              'top':location
+          }, this.FBS.speed);
+        }
+        else if('down' === this.FBS.effect){
 
-        $(child).stop().animate({
-            'top':targetTop
-        }, this.FBS.speed);
+          location = this.FBS.top + this.FBS.buttonDistance * count;
+          child = this.FBS.tag.parentNode.childNodes[count];
+          $(child).css('top',$(child).offset().top).css('z-index',100-count);
+          //effect up down
+          $(child).stop().animate({
+              'top':location
+          }, this.FBS.speed);
+        }
+        else if('right' === this.FBS.effect){
+          location = this.FBS.left - this.FBS.buttonDistance * count;
+          child = this.FBS.tag.parentNode.childNodes[count];
+          $(child).css('left',$(child).offset().left).css('z-index',100-count);
+          //effect up down
+          $(child).stop().animate({
+              'left':location
+          }, this.FBS.speed);
+        }
+        else if('left' === this.FBS.effect){
+          location = this.FBS.left + this.FBS.buttonDistance * count;
+          child = this.FBS.tag.parentNode.childNodes[count];
+          $(child).css('left',$(child).offset().left).css('z-index',100-count);
+          //effect up down
+          $(child).stop().animate({
+              'left':location
+          }, this.FBS.speed);
+        }
+
+
+
       }
       this.FBS.target
         .css('transition','all 0.3s')
@@ -52,12 +98,21 @@ var Main = React.createClass({
     }
     else{
       this.FBS.showFlag = false;
-
-      for(count ; count < this.FBS.childrenLength; count++){
-        $(this.FBS.tag.parentNode.childNodes[count]).stop().animate({
-            'top':this.FBS.top
-        }, this.FBS.speed);
+      if('up' === this.FBS.effect || 'down' === this.FBS.effect){
+        for(count ; count < this.FBS.childrenLength; count++){
+          $(this.FBS.tag.parentNode.childNodes[count]).stop().animate({
+              'top':this.FBS.top
+          }, this.FBS.speed);
+        }
       }
+      else if('right' === this.FBS.effect || 'left' === this.FBS.effect){
+        for(count ; count < this.FBS.childrenLength; count++){
+          $(this.FBS.tag.parentNode.childNodes[count]).stop().animate({
+              'left':this.FBS.left
+          }, this.FBS.speed);
+        }
+      }
+
       this.FBS.target.css('transition','all 0.3s').css('transform','rotateZ(0deg)');
     }
   },
@@ -68,13 +123,13 @@ var Main = React.createClass({
 
         <FloatingButtonContainer targetId='floatingPostion'>
           <FloatingButtonMain
-            style={{width:60,height:60,fontSize:30}} onClick={this.actionFloatButton} ></FloatingButtonMain>
+            style={this.FBS.buttonStyle} onClick={this.actionFloatButton} ></FloatingButtonMain>
           <FloatingButtonChildren
-            style={{width:60,height:60,fontSize:30}}
+            style={this.FBS.buttonStyle}
             iconClass='fa fa-bolt' onClick={this.actionChildren1}
             ></FloatingButtonChildren>
             <FloatingButtonChildren
-              style={{width:60,height:60,fontSize:30}} iconClass='fa fa-eye' onClick={this.actionChildren2}
+              style={this.FBS.buttonStyle} iconClass='fa fa-eye' onClick={this.actionChildren2}
               ></FloatingButtonChildren>
         </FloatingButtonContainer>
       </div>
